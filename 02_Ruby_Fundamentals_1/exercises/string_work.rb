16d425b8625f2ab99ffcb834379e5789a9efe777
William Rames <william@velvetsofa.com>
2015-08-03 20:22:10 -0700

    string work completed

---
+++ 02_Ruby_Fundamentals_1/exercises/string_work.rb
@@ -0,0 +1,8 @@
+greeting = "Hey dude!"
+name = "William"
+puts "the length of #{greeting} is #{greeting.length}"
+puts "the two variables combined is #{greeting.prepend(name)}"
+puts name << greeting
+name << "    "
+puts name.strip
+puts name.chars
